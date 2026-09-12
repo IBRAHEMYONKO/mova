7827e5fcdc4eed0dc03869402fa2d40fc0a5e9e3
IBRAHEMYONKO <131961473+IBRAHEMYONKO@users.noreply.github.com>
2026-09-12 16:59:22 +0300

    fix: render my list page from page builder

--- api/my-list-page.js
+++ api/my-list-page.js
@@ -1,18 +1,14 @@
 "use strict";
 
-const fs = require("node:fs");
-const path = require("node:path");
-const { injectMyListScripts } = require("../lib/my-list-page");
+const { buildMyListPage } = require("../lib/my-list-page");
 
 module.exports = (req, res) => {
     try {
-        const file = path.join(process.cwd(), "my-list.html");
-        const html = injectMyListScripts(fs.readFileSync(file, "utf8"));
         res.statusCode = 200;
         res.setHeader("Content-Type", "text/html; charset=utf-8");
         res.setHeader("Cache-Control", "no-store");
-        return res.end(html);
-    } catch (error) {
+        return res.end(buildMyListPage());
+    } catch {
         res.statusCode = 500;
         res.setHeader("Content-Type", "text/plain; charset=utf-8");
         return res.end("تعذر تحميل قائمة المستخدم");
