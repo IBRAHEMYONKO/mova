349cfeb48ab8dee4fd4c3efdc4e2238a360386c8
IBRAHEMYONKO <131961473+IBRAHEMYONKO@users.noreply.github.com>
2026-09-12 16:16:45 +0300

    test: define home page script injection

---
+++ test/home-page.test.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const assert = require("node:assert/strict");
+const test = require("node:test");
+
+const { injectNavigationScript } = require("../lib/home-page");
+
+test("injects the watch navigation script once", () => {
+    const html = "<html><body><main>OK</main></body></html>";
+    const output = injectNavigationScript(html);
+
+    assert.match(output, /<script src="watch-navigation\.js"><\/script>/);
+    assert.equal((output.match(/watch-navigation\.js/g) || []).length, 1);
+});
